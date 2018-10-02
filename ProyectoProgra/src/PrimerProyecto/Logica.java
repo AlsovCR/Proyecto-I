ecfd609f1509df13b2fc7faebcbcf7e198b97b0f
Alsov <Alsov@10.234.222.252>
2018-10-02 10:14:35 -0600

    Primeras clases proyecto

---
+++ ProyectoProgra/src/PrimerProyecto/Logica.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package PrimerProyecto;
+
+/**
+ * 
+ * @author Alsov
+ */
+
+public class Logica {
+
+    public int [][] generaMatriz (int jugada){
+        
+        int tablero [][] = new int [3][3];
+        
+          for ( int f = 0 ; f<tablero.length; f++){
+            
+            for (int c = 0 ; c<tablero[0].length; c++){
+                
+                tablero [f] [c] = jugada;
+                
+            }
+            
+        }
+        
+        return tablero;
+        
+    }
+    
+    public int ganaFilas (int tablero [][], int jugada){
+        
+        for ( int f = 0 ; f<tablero.length; f++){
+            
+            for (int c = 0 ; c<tablero[0].length; c++){
+                
+                
+                
+                
+            }
+            
+        }
+        
+        
+    }
+    
+    
+    
+}
+
+
+
+/* ganaFilas(int tablero[][], int jugada)
+-
+ganaColum
+nas(int tablero[][], int jugada)
+-
+ganaDiagonal(int tablero[][], int jugada)
+-
+ganaDiagonalnversa(int tablero[][], int jugada)
+-
+ganaJugadorUno(int tablero[][])
+-
+gan
+aJugadorDos(int tablero[][])  */

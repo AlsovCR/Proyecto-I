3d18caaa11402906c9371b51c6b9eeca04839721
HP15 <HP15@10.234.223.201>
2018-10-03 18:13:24 -0600

    arreglo interfaz

--- ProyectoProgra/src/PrimerProyecto/Interfaz.java
+++ ProyectoProgra/src/PrimerProyecto/Interfaz.java
@@ -4,6 +4,7 @@ package PrimerProyecto;
 
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
@@ -13,35 +14,51 @@ import javafx.scene.layout.GridPane;
 
 public class Interfaz {
     
-    public BorderPane borderPane (){
-        BorderPane inicial = new BorderPane();
-        inicial.setLeft(inicio());
-        return inicial;
-    }
     public GridPane inicio(){
          GridPane conten = new GridPane();
-        conten.setVgap(5);
-        conten.add(new Label("Proyecto del curso IF2000 \n Estudiantes: \n Graciela Porras Rojas B75923 \n Allen Solano"), 0, 1);
-        conten.add(new Label("Descripción del Juego: \n Es un juego en el que dos jugadores se turnan para para colocar su figura (O o X) en una casilla vacía del tablero. \n El ganador es el primer jugador que obtiene tres de los mismos símbolos en una fila vertical, horizontal o diagonal.\n En el caso de que se ocupen todas las casillas y ningún jugador haya realizado una línea recta, se declara empate \n No se puede: cambiar de figura ni colocar una figura en una casilla ya ocupada por cualquier figura."), 0,2);
-        conten.add(new Button("Comenzar Juego"), 0, 6);
-//         
-//         btnComenzar.setOnAction(new EventHandler<ActionEvent>() {
-//
-//            public void handle(ActionEvent evento) {
-//                        inicial.setLeft(JuegoGato());
-//                    
-//            }
-//         });
-//        
+         
+         conten.setVgap(10);
+         conten.setHgap(10);
+         Label lbInformación = new Label("Proyecto del curso IF2000 \n Estudiantes: \n Graciela Porras Rojas B75923 \n Allen Solano");
+         GridPane.setConstraints((lbInformación),0,1);
+         
+         Label lbDescripción = new Label ("Descripción del Juego: \n Es un juego en el que dos jugadores se turnan para para colocar su figura (O o X) en una casilla vacía del tablero. \n El ganador es el primer jugador que obtiene tres de los mismos símbolos en una fila vertical, horizontal o diagonal.\n En el caso de que se ocupen todas las casillas y ningún jugador haya realizado una línea recta, se declara empate \n No se puede: cambiar de figura ni colocar una figura en una casilla ya ocupada por cualquier figura.");
+         GridPane.setConstraints((lbDescripción),0,3);
+         
+         Button btnComenzar= new Button ("Comenzar a Jugar");
+         GridPane.setConstraints((btnComenzar), 0, 5);
+         
+         btnComenzar.setOnAction(new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent evento) {
+            }
+         });
+     
+        conten.getChildren().addAll(lbInformación, lbDescripción, btnComenzar);
+        
         return conten;
     }
      public GridPane JuegoGato(){
          GridPane contenedor = new GridPane();
-        contenedor.setVgap(10);
-        contenedor.add(new Button("Nuevo Juego"), 0, 2);
-        contenedor.add(new Button("Reiniciar"), 0, 3);
-        contenedor.add(new Button("Créditos"), 0, 4);
+         contenedor.setVgap(10);
+         contenedor.setHgap(10);
+         
+         Button nuevojuego= new Button ("Nuevo Juego");
+         GridPane.setConstraints((nuevojuego), 0, 2);
+         
+         Button reiniciar= new Button ("Reiniciar");
+         GridPane.setConstraints((reiniciar), 0, 4);
+         
+         Button créditos= new Button ("Créditos");
+         GridPane.setConstraints((créditos), 0, 5);
+       
+        contenedor.getChildren().addAll(nuevojuego, reiniciar, créditos);
         
         return contenedor;
      }
+//     public GridPane tablero(){
+//         GridPane tablerogato = new GridPane();
+//         
+//        
+//         
+//     }
 }
